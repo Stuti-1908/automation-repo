4bffc468ad2c7a21d0968a78d81fcf04add8339f
Stuti Gohil <0144.stuti.svmgirlsg@gmail.com>
2025-06-28 01:56:45 +0530

    visual and slector automation workflow working fine

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -3,50 +3,108 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "..\\bitmaps_reference\\ui-visual-regression_Home_Page_0_document_0_desktop.png",
-        "test": "..\\bitmaps_test\\20250626-021850\\ui-visual-regression_Home_Page_0_document_0_desktop.png",
+        "reference": "..\\bitmaps_reference\\backstop_default_Home_Page_0_document_0_phone.png",
+        "test": "..\\bitmaps_test\\20250627-204212\\backstop_default_Home_Page_0_document_0_phone.png",
         "selector": "document",
-        "fileName": "ui-visual-regression_Home_Page_0_document_0_desktop.png",
+        "fileName": "backstop_default_Home_Page_0_document_0_phone.png",
         "label": "Home Page",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "url": "http://localhost:5173/",
         "expect": 0,
-        "viewportLabel": "desktop",
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -64,
+            "height": -229
+          },
+          "rawMisMatchPercentage": 55.91575575928538,
+          "misMatchPercentage": "55.92",
+          "analysisTime": 120
+        },
+        "diffImage": "..\\bitmaps_test\\20250627-204212\\failed_diff_backstop_default_Home_Page_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Home_Page_0_document_1_tablet.png",
+        "test": "..\\bitmaps_test\\20250627-204212\\backstop_default_Home_Page_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Home_Page_0_document_1_tablet.png",
+        "label": "Home Page",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:5173/",
+        "expect": 0,
+        "viewportLabel": "tablet",
         "diff": {
           "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.00"
-        }
+          "rawMisMatchPercentage": 42.60495503743489,
+          "misMatchPercentage": "42.60",
+          "analysisTime": 136
+        },
+        "diffImage": "..\\bitmaps_test\\20250627-204212\\failed_diff_backstop_default_Home_Page_0_document_1_tablet.png"
       },
-      "status": "pass"
+      "status": "fail"
     },
     {
       "pair": {
-        "reference": "..\\bitmaps_reference\\ui-visual-regression_Settings_Page_0_document_0_desktop.png",
-        "test": "..\\bitmaps_test\\20250626-021850\\ui-visual-regression_Settings_Page_0_document_0_desktop.png",
+        "reference": "..\\bitmaps_reference\\backstop_default_Settings_Page_0_document_0_phone.png",
+        "test": "..\\bitmaps_test\\20250627-204212\\backstop_default_Settings_Page_0_document_0_phone.png",
         "selector": "document",
-        "fileName": "ui-visual-regression_Settings_Page_0_document_0_desktop.png",
+        "fileName": "backstop_default_Settings_Page_0_document_0_phone.png",
         "label": "Settings Page",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "url": "http://localhost:5173/settings",
         "expect": 0,
-        "viewportLabel": "desktop",
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -64,
+            "height": -229
+          },
+          "rawMisMatchPercentage": 55.91575575928538,
+          "misMatchPercentage": "55.92",
+          "analysisTime": 143
+        },
+        "diffImage": "..\\bitmaps_test\\20250627-204212\\failed_diff_backstop_default_Settings_Page_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Settings_Page_0_document_1_tablet.png",
+        "test": "..\\bitmaps_test\\20250627-204212\\backstop_default_Settings_Page_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Settings_Page_0_document_1_tablet.png",
+        "label": "Settings Page",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:5173/settings",
+        "expect": 0,
+        "viewportLabel": "tablet",
         "diff": {
           "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.00"
-        }
+          "rawMisMatchPercentage": 42.60495503743489,
+          "misMatchPercentage": "42.60",
+          "analysisTime": 131
+        },
+        "diffImage": "..\\bitmaps_test\\20250627-204212\\failed_diff_backstop_default_Settings_Page_0_document_1_tablet.png"
       },
-      "status": "pass"
+      "status": "fail"
     }
   ],
-  "id": "ui-visual-regression"
+  "id": "backstop_default"
 });
